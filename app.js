caa627908892b27f1f5698b7b7de330eb79164e3
Alan Burga <ahbb21@hotmail.com>
2022-09-01 21:41:39 -0500

    consigna 1

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 import express from "express";
-
-import Products from "./classes/products.js";
+import http from "http";
+import { Server } from "socket.io";
 import routerProduct from "./router/products.router.js";
-//import routerCart from "./router/cart.router.js";
-
-import path from "path";
+import routerProductTest from "./router/productsTest.router.js";
+import ContenedorChat from "./classes/ContenedorChatFile.js";
 
 const app = express();
+
+const server = http.createServer(app);
+const io = new Server(server);
+
 app.use(express.json());
+const db = new ContenedorChat("db.json");
 //Template Engines
 
 app.use(express.urlencoded({ extended: true }));
@@ -24,7 +28,31 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/productos", routerProduct);
+app.use("/api/productos-test", routerProductTest);
+app.use("/chat", (req, res) => {
+  res.render("index");
+});
 
-//app.get("/", (req, res) => res.send(productos));
+io.on("connection", (socket) => {
+  console.log("Somebody connected!");
 
-app.listen(8080);
+  socket.on("chat-in", (data) => {
+    const date = new Date().toLocaleTimeString();
+    const dataOut = {
+      msn: data.msn,
+      username: data.username,
+      date,
+    };
+    // Guardar en DB
+    db.save(dataOut);
+
+    console.log(dataOut);
+    io.sockets.emit("chat-out", dataOut);
+  });
+});
+
+const PORT = process.env.PORT || 8080;
+
+server.listen(PORT, () => {
+  console.log("Running...");
+});
